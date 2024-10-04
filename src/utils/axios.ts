4f0522f6ab3f1ddc174bc467809deac54e09f79f
Muhammed Rafi <rafikandathuvayal@gmail.com>
2024-10-04 15:07:19 +0530

    url changed and status updated

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from "axios";
 
 
-const BASE_URL = 'http://localhost:5500'
+const BASE_URL = 'https://worknest-server.onrender.com'
 
 
 const apiClient = axios.create({
